008a6a742ecece20e05679190c6d5e79c83cf95d
steve <steven.thompson.ca@gmail.com>
2016-11-07 10:00:49 -0500

    Initial Commit

---
+++ Demo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo2
+//
+//  Created by steve on 2016-11-07.
+//  Copyright © 2016 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
